1206b189c5d54641da9080bb354f4520c19c14d3
Maulick Bharadwaj <Maulick.bharadwaj@Geminisolutions.com>
2023-05-26 14:36:29 +0530

    hi

--- src/main/java/StepDefination/Hook.java
+++ src/main/java/StepDefination/Hook.java
@@ -2,16 +2,20 @@ package StepDefination;
 
 import com.gemini.generic.exception.GemException;
 import com.gemini.generic.reporting.GemEcoUpload;
+import com.gemini.generic.reporting.GemTestReporter;
+import com.gemini.generic.reporting.STATUS;
 import com.gemini.generic.ui.utils.DriverAction;
 import com.gemini.generic.ui.utils.DriverManager;
 import com.gemini.generic.utils.GemJarGlobalVar;
 import com.gemini.generic.utils.GemJarUtils;
 import io.cucumber.java.Before;
 import com.gemini.generic.reporting.GemTestReporter.*;
+import io.cucumber.java.BeforeAll;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class Hook {
@@ -27,5 +31,16 @@ public class Hook {
 //        DriverAction.setPageLoadTimeOut(Long.parseLong(GemJarGlobalVar.pageTimeout));
 //        DriverAction.setScriptTimeOut(Long.parseLong(GemJarGlobalVar.scriptTimeout));
     }
+
+    @BeforeAll
+    public static void hook2() throws GemException {
+        Map<String, String> envVars = System.getenv();
+        for (String key : envVars.keySet()) {
+            String value = envVars.get(key);
+            System.out.println(key + " = " + value);
+//            GemTestReporter.addTestStep("env variables", key + " = " + value, STATUS.INFO);
+        }
+    }
+
     private final static Logger logger = Logger.getLogger(String.valueOf(GemEcoUpload.class));
 }
